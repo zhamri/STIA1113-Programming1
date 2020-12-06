2418e243831d36ef7e296bef16a0283af6d1ff2a
zhamri <zhamri@gmail.com>
2020-12-06 13:12:05 +0800

    example increment

---
+++ src/main/java/my/zhamri/variable/Example_Increment.java
@@ -0,0 +1,15 @@
+package my.zhamri.variable;
+
+public class Example_Increment {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int i = 2;
+
+        x = i++;
+
+        System.out.println("x = " + x);
+        System.out.println("i = " + i);
+    }
+}
